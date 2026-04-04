c6362b27217ff498ad816ccbd8ff3af122971bd0
mohammad hafreed <hafreed02@gmail.com>
2026-04-05 01:04:13 +0530

    Time: 30 ms (72.26%), Space: 146.7 MB (66.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,37 +2,17 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         int[] arr=new int[n-k+1];
-        int i=0;
-        int j=0;
-        int index=-1;
-        int ind=0;
-        int fmax=Integer.MIN_VALUE;
-        while(j<n)
+        int index=0;
+        Deque<Integer> dq=new ArrayDeque<>();
+        for(int i=0;i<n;i++)
         {
-            if(nums[j]>fmax)
-            {
-                fmax=nums[j];
-                index=j;
-            }
-            if(j-i+1==k)
-            {
-                arr[ind++]=fmax;
-                if(index==i)
-                {
-                    fmax=Integer.MIN_VALUE;
-                    for(int l=i+1;l<=j;l++)
-                    {
-                        if(nums[l]>fmax)
-                        {
-                            fmax=nums[l];
-                            index=l;
-                        }
-                    }  
-                }
-                i++;
-            }
-            j++;
-
+            if(!dq.isEmpty()&&dq.peekFirst()==i-k)
+            dq.removeFirst();
+            while(!dq.isEmpty()&&nums[dq.peekLast()]<nums[i])
+            dq.removeLast();    
+            dq.addLast(i);
+            if(i>=k-1)
+            arr[index++]=nums[dq.peekFirst()];
         }
         return arr;
     }
